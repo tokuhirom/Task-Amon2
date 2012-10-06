e4b97593aeb102fc89644b28fcf8e4cb3ca51377
tokuhirom <tokuhirom@gmail.com>
2012-10-06 17:52:09 +0900

    initial import

---
+++ lib/Task/Amon2.pm
@@ -0,0 +1,39 @@
+package Task::Amon2;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Amon2 - A module for you
+
+=head1 SYNOPSIS
+
+  use Task::Amon2;
+
+=head1 DESCRIPTION
+
+Task::Amon2 is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
